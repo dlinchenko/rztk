967e8c45707df0bb8ad7596a86c61bd1844623bf
Dmytro Linchenko <dmytro_linchenko@epam.com>
2018-03-20 22:11:16 +0200

    removed unnecessary import

--- src/main/java/ExcelHelper.java
+++ src/main/java/ExcelHelper.java
@@ -10,7 +10,6 @@ import java.lang.String;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 class ExcelHelper {
     private HSSFWorkbook workbook;
